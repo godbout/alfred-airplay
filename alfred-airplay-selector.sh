263f50d6bdd09392ffb721b6cda367c8cae403ea
Guillaume <guill.bout@gmail.com>
2017-12-12 00:34:15 +0800

    Add better notifications (when cannot find display)

--- alfred-airplay-selector.sh
+++ alfred-airplay-selector.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 read -d '' APPLESCRIPT <<EOF
+set found to false
+
 tell application "System Events"
     tell process "SystemUIServer"
         click (menu bar item 1 of menu bar 1 whose description contains "Displays")
         click menu item "$1" of menu 1 of result
+        set found to true
         delay 4
     end tell
 end tell
@@ -14,6 +17,8 @@ osascript -e "$APPLESCRIPT" > /dev/null
 
 if [[ "$1" = "Turn AirPlay Off" ]]; then
     echo "Display not shared anymore";
+elif [ found = true ]; then
+    echo "Display now shared with \"$1\"";
 else
-    echo "Cannot find display to share with";
-endif 
+    echo "Cannot find \"$1\" display";
+fi
