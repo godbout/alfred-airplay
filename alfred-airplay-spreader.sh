0acf18a20eaba938d14c8561fe91fdca83d99dce
Guillaume <guill.bout@gmail.com>
2017-12-14 05:34:17 +0800

    Fix issue with notification + handle non existing display better

--- alfred-airplay-spreader.sh
+++ alfred-airplay-spreader.sh
@@ -2,22 +2,26 @@
 
 read -d '' APPLESCRIPT <<EOF
 set found to false
-
 tell application "System Events"
     tell process "SystemUIServer"
         click (menu bar item 1 of menu bar 1 whose description contains "Displays")
-        click menu item "$1" of menu 1 of result
-        set found to true
-        delay 4
+        try
+            click menu item "$1" of menu 1 of result
+            set found to true
+            delay 4
+        on error
+            key code 53
+        end try
     end tell
 end tell
+return found
 EOF
 
-osascript -e "$APPLESCRIPT" > /dev/null
+found=$(osascript -e "$APPLESCRIPT")
 
 if [[ "$1" = "Turn AirPlay Off" ]]; then
     echo "Display not shared anymore";
-elif [ found = true ]; then
+elif [ "$found" = true ]; then
     echo "Display now shared with \"$1\"";
 else
     echo "Cannot find \"$1\" display";
