74fc2b0afb9144d97cec3c2e6bb5027058e56c3f
Guill Lo <guill.bout@gmail.com>
2020-01-25 01:59:42 +0800

    Improve notification system

--- aps-launch.sh
+++ aps-launch.sh
@@ -8,7 +8,7 @@ tell application "System Events"
         try
             click menu item "$1" of menu 1 of result
             set found to true
-            delay 4
+            delay 1
         on error
             key code 53
         end try
@@ -26,7 +26,7 @@ if [[ "$1" = "Turn AirPlay Off" ]]; then
         echo "Cannot stop sharing display";
     fi
 elif [ "$found" = true ]; then
-    echo "Display now shared with \"$1\"";
+    echo "Display will now be shared with \"$1\"";
 else
     echo "Cannot find \"$1\" display";
 fi
